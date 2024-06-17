6ca286e5869c8c8a9eac96ba53f96d3d7dcfea45
mthej59 <mthej59@gamil.com>
2024-06-17 17:52:00 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<div>
+				<Header />
+			</div>
 			<div>
 				<a
 					href='https://vitejs.dev'
@@ -34,7 +39,7 @@ function App() {
 			<div className='card'>
 				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
+					Edit <code>src/App.jsx</code> and save to test HMR!!!!
 				</p>
 			</div>
 			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
