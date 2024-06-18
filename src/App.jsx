a264609f0fe890fbb3f26fc093f8ae9ed79da9cc
mthej59 <mthej59@gamil.com>
2024-06-18 13:13:45 +0900

    Main CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import "./App.css";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import './App.css';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Button from './components/Button';
 import Header from './components/Header';
 
 function App() {
-  return (
-    <>
-      <Header>
-        <div>
-          <Button text={"검색"} />
-        </div>
-      </Header>
-      <div>
-      <Main />
-      </div>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header>
+				<div>
+					<Button text={'검색'} />
+				</div>
+			</Header>
+			<div>
+				<Main>
+          
+        </Main>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
+import './Footer.css';
 const Footer = () => {
-  return (
-    <footer>
-      <h3>© 2024 Bitcamp, Inc.  </h3>  
-      <p>개인정보처리방침   이용약관   사이트맵   회사 세부정보   위치기반서비스 이용약관   이용자보호   비전과 계획   청소년보호정책</p> 
-      <p>고객센터 : 02-xxxx-xxxx, bitcamp@test.com </p>
-    </footer>
-  );
-}
-
+	return (
+		<footer>
+			<h3>© 2024 Bitcamp, Inc. </h3>
+			<p>
+				개인정보처리방침 이용약관 사이트맵 회사 세부정보 위치기반서비스 이용약관 이용자보호 비전과 계획 청소년보호정책
+			</p>
+			<p>고객센터 : 02-xxxx-xxxx, bitcamp@test.com </p>
+		</footer>
+	);
+};
 export default Footer;
