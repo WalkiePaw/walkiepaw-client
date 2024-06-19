56c5f66be12a2bba8ac4e9bdfae1ff7a79068f2c
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-19 19:48:43 +0900

    .

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,7 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Button from './pages/Button';
-import { Routes, Route, Link, useNavigate } from "react-router-dom"
 
 export default function Layout() {
   return (
