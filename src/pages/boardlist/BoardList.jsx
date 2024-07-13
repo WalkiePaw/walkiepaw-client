8c5670921f1f1612f670f8212d511424b5b6a3cc
mthej59 <mthej59@gamil.com>
2024-07-14 01:59:22 +0900

    BoardList 페이지네이션, 무한스크롤 & css 수정

--- src/pages/boardlist/BoardList.jsx
+++ src/pages/boardlist/BoardList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
+
 import CardList from "../../components/cardList/CardList";
 import "./BoardList.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -24,25 +25,11 @@ const BoardList = () => {
   const [page, setPage] = useState(0); // 기본값 1로 바꾸기
   const [hasMore, setHasMore] = useState(true);
   const { user } = useSelector((state) => state.auth); // 로그인한 유저의 정보를 가져온다
-  const [loading, setLoading] = useState(false); // 게시글이 출력되기 전 상태 표시
-
-  console.log("====>", user);
-
+  const [loading, setLoading] = useState(true); // 게시글이 출력되기 전 상태 표시
   const location = useLocation(); // 현재 경로 정보를 가져오는 Hook
   const navigate = useNavigate(); // 페이지 이동을 위한 함수
   const [memberPhoto, setMemberPhoto] = useState(null);
   const [memberId, setMemberId] = useState(null);
-
-  // JWT 토큰을 디코딩하여 사용자 정보를 추출
-  useEffect(() => {
-    const token = localStorage.getItem("jwtToken");
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      console.log("JWT 토큰", decodedToken);
-      setMemberId(decodedToken.id); // 사용자 ID를 상태에 저장
-      console.log("회원번호:", decodedToken.id);
-    }
-  }, []);
 
   // location의 값에서 '동'이 포함된 값만 추출
   const dongFromLocal = (location) => {
@@ -60,12 +47,12 @@ const BoardList = () => {
     }
     // 페이지의 카테고리가 변경 될 때 마다 검색 키워드 초기화
     setSearchKeyword("");
-    setFilteredPosts("");
+    setFilteredPosts([]);
     setSearchResultMessage("");
   }, [location.pathname]);
 
   // 카테고리가 변경되면 해당 게시글의 정보를 가져와 게시글의 정보를 다시 저장
-  const fetchPosts = useCallback(async () => {
+  const fetchPosts = async () => {
     try {
       if (!category || !hasMore) return; // 카테고리가 없으면 요청하지 않도록 처리
       setLoading(true);
@@ -75,41 +62,27 @@ const BoardList = () => {
           params: { page },
         }
       ); // 엔드포인트는 백엔드 서버의 게시글 목록을 반환해야 합니다.
-      console.log(response);
       const data = response?.data?.content ?? [];
       setPosts((prevPosts) => [...prevPosts, ...data]);
-      setHasMore(response.data?.last === false);
-      // setPage((prevPage) => prevPage + 1);
-    } catch (error) {
-      console.error("Failed to fetch posts", error);
-    } finally {
+      setFilteredPosts((prevPosts) => [...prevPosts, ...data]);
+      setHasMore(!response.data.last);
+      setPage((prevState) => prevState + 1);
       setLoading(false);
+      console.log("page!!!!!", page);
+    } catch (error) {
+      setLoading(false);
+      console.error("Failed to fetch posts", error);
     }
-  }, [category, hasMore, page]);
+  };
 
-  useEffect(() => {
-    fetchPosts();
-  }, [category, fetchPosts]);
-
-  // 무한 스크롤
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + document.documentElement.scrollTop >=
-          document.documentElement.offsetHeight - 100 &&
-        hasMore
-      ) {
-        setPage((prevPage) => prevPage + 1);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [hasMore]);
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, [category, fetchPosts]);
 
   // 카테고리, 지역, 검색어 필터링
   useEffect(() => {
     const filterPosts = () => {
-      if (!posts) return; // 게시글이 없으면 필터링을 하지 않음
+      if (posts.length === 0) return; // 게시글이 없으면 필터링을 하지 않음
 
       let newFilteredPosts = posts;
 
@@ -180,7 +153,7 @@ const BoardList = () => {
       setPosts(response.data?.content);
       setFilteredPosts(response?.data?.content);
       setHasMore(response.data?.last === false);
-      setPage(1);
+      setPage((prevState) => prevState + 1);
     } catch (error) {
       console.error("검색 요청 실패!", error);
       alert("검색에 실패 했습니다.");
@@ -227,19 +200,25 @@ const BoardList = () => {
 
   useEffect(() => {
     let observer;
-    if (target) {
+    console.log("@@@@==>>나 실행됨");
+    // if (posts.length === 0) {
+    //   return;
+    // }
+    if (ref.current) {
       const onIntersect = async ([entry], observer) => {
         if (entry.isIntersecting) {
-          observer.unobserve(entry.target);
-          await fetchData();
-          observer.observe(entry.target);
+          await fetchPosts();
+          console.log("나 패치포스트@@@@ =>", 11111);
         }
       };
-      observer = new IntersectionObserver(onIntersect, { threshold: 1 }); // 추가된 부분
-      observer.observe(target);
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.1 }); // 추가된 부분
+      observer.observe(ref.current);
     }
     return () => observer && observer.disconnect();
-  }, [target]);
+  }, [category, page]);
+
+  console.log("filteredPosts", filteredPosts);
+  console.log("posts", posts);
 
   return (
     <>
@@ -276,32 +255,42 @@ const BoardList = () => {
           filteredPosts?.length === 0 && !loading ? "no-posts-container" : ""
         }`}
       >
-        {loading ? (
+        {loading && page === 0 ? (
           <div className="loading">Loading...</div>
         ) : filteredPosts?.length > 0 ? (
-          filteredPosts.map((post) => (
-            <div key={post.id} onClick={() => handlePostClick(post)}>
-              <CardList
-                boardId={post.id}
-                title={post.title}
-                location={dongFromLocal(post.location)}
-                price={post.price}
-                priceType={post.priceType}
-                startTime={post.startTime}
-                endTime={post.endTime}
-                photoUrls={post.photoUrls}
-                memberNickName={post.memberNickName}
-                status={post.status} // 구인중, 구인 대기중, 구인 완료 등 상태 정보
-                category={post.category} // 카테고리 정보 전달
-                memberPhoto={post.memberPhoto}
-                id={memberId} // 로그인한 사용자의 ID
-                initialLikeCount={post.likeCount} // 여기서 likeCount 전달
-              />
-            </div>
-          ))
+          <div className="contents">
+            {filteredPosts.map((post) => (
+              <div key={post.id} onClick={() => handlePostClick(post)}>
+                <CardList
+                  boardId={post.id}
+                  title={post.title}
+                  location={dongFromLocal(post.location)}
+                  price={post.price}
+                  priceType={post.priceType}
+                  startTime={post.startTime}
+                  endTime={post.endTime}
+                  photoUrls={post.photoUrls}
+                  memberNickName={post.memberNickName}
+                  status={post.status} // 구인중, 구인 대기중, 구인 완료 등 상태 정보
+                  category={post.category} // 카테고리 정보 전달
+                  memberPhoto={post.memberPhoto}
+                  id={memberId ?? 0} // 로그인한 사용자의 ID
+                  initialLikeCount={post.likeCount} // 여기서 likeCount 전달
+                />
+              </div>
+            ))}
+            {/* {loading && <div>Loading...</div>} 페이지가 로딩중인걸 보고싶다면 주석을 풀어라 */}
+          </div>
         ) : (
           <div className="no-posts">게시글이 없습니다.</div>
         )}
+        <div
+          ref={ref}
+          style={{
+            height: "10px",
+            width: "100%",
+          }}
+        />
         <div className="bl-button-container">
           {user && (
             <Link to="/new-post">
@@ -315,7 +304,6 @@ const BoardList = () => {
           </button>
         </div>
       </div>
-      <div ref={ref}>여기에 스크롤을 내리면 "안녕"이라는 알림이 뜹니다.</div>
     </>
   );
 };
