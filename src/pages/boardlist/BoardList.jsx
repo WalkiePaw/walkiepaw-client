39c62d2262205f66255e3f68b11a9e36865661c5
mthej59 <mthej59@gamil.com>
2024-07-13 23:18:47 +0900

    작업중.,.

--- src/pages/boardlist/BoardList.jsx
+++ src/pages/boardlist/BoardList.jsx
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import CardList from '../../components/cardList/CardList';
-import './BoardList.css';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import MyTown from '../myTown/MyTown';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { useSelector } from 'react-redux';
-import { jwtDecode } from 'jwt-decode';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import CardList from "../../components/cardList/CardList";
+import "./BoardList.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import MyTown from "../myTown/MyTown";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
+import { jwtDecode } from "jwt-decode";
 
 const BoardList = () => {
+  const ref = useRef(null);
   const [posts, setPosts] = useState([]); // 게시글 목록을 저장
   const [filteredPosts, setFilteredPosts] = useState([]); // 필터링된 게시글 목록을 저장
-  const [selectedSi, setSelectedSi] = useState('');
-  const [selectedGu, setSelectedGu] = useState('');
-  const [selectedDong, setSelectedDong] = useState('');
-  const [searchKeyword, setSearchKeyword] = useState(''); // 검색어를 저장
-  const [searchOption, setSearchOption] = useState('title'); // 검색 옵션(제목 or 내용) 디폴트 값
-  const [category, setCategory] = useState(''); // 게시글 카테고리를 저장
-  const [searchResultMessage, setSearchResultMessage] = useState(''); // 검색 결과 메시지
-  const [page, setPage] = useState(0);
+  const [selectedSi, setSelectedSi] = useState("");
+  const [selectedGu, setSelectedGu] = useState("");
+  const [selectedDong, setSelectedDong] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState(""); // 검색어를 저장
+  const [searchOption, setSearchOption] = useState("title"); // 검색 옵션(제목 or 내용) 디폴트 값
+  const [category, setCategory] = useState(""); // 게시글 카테고리를 저장
+  const [searchResultMessage, setSearchResultMessage] = useState(""); // 검색 결과 메시지
+  const [page, setPage] = useState(0); // 기본값 1로 바꾸기
   const [hasMore, setHasMore] = useState(true);
   const { user } = useSelector((state) => state.auth); // 로그인한 유저의 정보를 가져온다
   const [loading, setLoading] = useState(false); // 게시글이 출력되기 전 상태 표시
 
-  // console.log('====>', user);
+  console.log("====>", user);
 
   const location = useLocation(); // 현재 경로 정보를 가져오는 Hook
   const navigate = useNavigate(); // 페이지 이동을 위한 함수
@@ -34,7 +35,7 @@ const BoardList = () => {
 
   // JWT 토큰을 디코딩하여 사용자 정보를 추출
   useEffect(() => {
-    const token = localStorage.getItem('jwtToken');
+    const token = localStorage.getItem("jwtToken");
     if (token) {
       const decodedToken = jwtDecode(token);
       console.log("JWT 토큰", decodedToken);
@@ -52,54 +53,57 @@ const BoardList = () => {
   // 페이지 경로에 따라 카테고리를 설정
   useEffect(() => {
     const pathName = location.pathname;
-    if (pathName.includes('recruit')) {
-      setCategory('JOB_OPENING');
-    } else if (pathName.includes('jobs')) {
-      setCategory('JOB_SEARCH');
+    if (pathName.includes("recruit")) {
+      setCategory("JOB_OPENING");
+    } else if (pathName.includes("jobs")) {
+      setCategory("JOB_SEARCH");
     }
     // 페이지의 카테고리가 변경 될 때 마다 검색 키워드 초기화
-    setSearchKeyword('');
-    setFilteredPosts('');
-    setSearchResultMessage('');
+    setSearchKeyword("");
+    setFilteredPosts("");
+    setSearchResultMessage("");
   }, [location.pathname]);
 
   // 카테고리가 변경되면 해당 게시글의 정보를 가져와 게시글의 정보를 다시 저장
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        if (!category || !hasMore) return; // 카테고리가 없으면 요청하지 않도록 처리
-        setLoading(true);
-        const response = await axios.get(`http://localhost:8080/api/v1/boards/list/${category}`, {
-          params: { page, size: 8 },
-        }); // 엔드포인트는 백엔드 서버의 게시글 목록을 반환해야 합니다.
-        console.log(response);
-        const data = response?.data?.content ?? [];
-        setPosts((prevPosts) => [...prevPosts, ...data]);
-        setHasMore(response.data?.last === false);
-        setPage((prevPage) => prevPage + 1);
-        setMemberPhoto(memberPhoto);
-      } catch (error) {
-        console.error('Failed to fetch posts', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchPosts = useCallback(async () => {
+    try {
+      if (!category || !hasMore) return; // 카테고리가 없으면 요청하지 않도록 처리
+      setLoading(true);
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/boards/list/${category}`,
+        {
+          params: { page },
+        }
+      ); // 엔드포인트는 백엔드 서버의 게시글 목록을 반환해야 합니다.
+      console.log(response);
+      const data = response?.data?.content ?? [];
+      setPosts((prevPosts) => [...prevPosts, ...data]);
+      setHasMore(response.data?.last === false);
+      // setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error("Failed to fetch posts", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [category, hasMore, page]);
 
+  useEffect(() => {
     fetchPosts();
-  }, [category, page, hasMore, memberPhoto]);
+  }, [category, fetchPosts]);
 
   // 무한 스크롤
   useEffect(() => {
     const handleScroll = () => {
       if (
-        window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 100 &&
+        window.innerHeight + document.documentElement.scrollTop >=
+          document.documentElement.offsetHeight - 100 &&
         hasMore
       ) {
         setPage((prevPage) => prevPage + 1);
       }
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [hasMore]);
 
   // 카테고리, 지역, 검색어 필터링
@@ -112,13 +116,19 @@ const BoardList = () => {
       // 지역 필터링
       if (selectedSi) {
         // location이 Null일 때, includes()를 호출한다.
-        newFilteredPosts = newFilteredPosts.filter((post) => post.location?.includes(selectedSi));
+        newFilteredPosts = newFilteredPosts.filter((post) =>
+          post.location?.includes(selectedSi)
+        );
       }
       if (selectedGu) {
-        newFilteredPosts = newFilteredPosts.filter((post) => post.location?.includes(selectedGu));
+        newFilteredPosts = newFilteredPosts.filter((post) =>
+          post.location?.includes(selectedGu)
+        );
       }
       if (selectedDong) {
-        newFilteredPosts = newFilteredPosts.filter((post) => post.location?.includes(selectedDong));
+        newFilteredPosts = newFilteredPosts.filter((post) =>
+          post.location?.includes(selectedDong)
+        );
       }
 
       setFilteredPosts(newFilteredPosts);
@@ -146,20 +156,23 @@ const BoardList = () => {
         size: 12,
       };
 
-      if (searchOption === 'title') {
+      if (searchOption === "title") {
         params.title = searchKeyword;
-      } else if (searchOption === 'content') {
+      } else if (searchOption === "content") {
         params.content = searchKeyword;
       }
 
       setLoading(true);
 
-      const response = await axios.get(`http://localhost:8080/api/v1/boards/search`, { params });
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/boards/search`,
+        { params }
+      );
       console.log(response.data);
 
       // 검색 결과가 없는 경우 처리
       if (response.data?.length === 0) {
-        alert('검색 결과가 없습니다.');
+        alert("검색 결과가 없습니다.");
         setLoading(false);
         return;
       }
@@ -169,15 +182,15 @@ const BoardList = () => {
       setHasMore(response.data?.last === false);
       setPage(1);
     } catch (error) {
-      console.error('검색 요청 실패!', error);
-      alert('검색에 실패 했습니다.');
+      console.error("검색 요청 실패!", error);
+      alert("검색에 실패 했습니다.");
     } finally {
       setLoading(false);
     }
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
       handleSearch();
     }
@@ -200,17 +213,33 @@ const BoardList = () => {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   // 카테고리에 따라 다른 페이지 타이틀 출력
   let pageTitle;
-  if (category === 'JOB_OPENING') {
-    pageTitle = '함께 걷는 행복, 반려견 산책의 모든 것';
-  } else if (category === 'JOB_SEARCH') {
-    pageTitle = '다양한 반려견 산책 동행을 찾아보세요';
+  if (category === "JOB_OPENING") {
+    pageTitle = "함께 걷는 행복, 반려견 산책의 모든 것";
+  } else if (category === "JOB_SEARCH") {
+    pageTitle = "다양한 반려견 산책 동행을 찾아보세요";
   }
+
+  useEffect(() => {
+    let observer;
+    if (target) {
+      const onIntersect = async ([entry], observer) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          await fetchData();
+          observer.observe(entry.target);
+        }
+      };
+      observer = new IntersectionObserver(onIntersect, { threshold: 1 }); // 추가된 부분
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
 
   return (
     <>
@@ -219,9 +248,17 @@ const BoardList = () => {
         <img src="img/dog3.jpg" className="listTop-img" alt="반려견 산책" />
       </div>
       <div className="filter-container">
-        <MyTown onSiChange={setSelectedSi} onGuChange={setSelectedGu} onDongChange={setSelectedDong} />
+        <MyTown
+          onSiChange={setSelectedSi}
+          onGuChange={setSelectedGu}
+          onDongChange={setSelectedDong}
+        />
         <div className="board-search-container">
-          <select className="search-filter" value={searchOption} onChange={handleOptionChange}>
+          <select
+            className="search-filter"
+            value={searchOption}
+            onChange={handleOptionChange}
+          >
             <option value="title">제목</option>
             <option value="content">내용</option>
           </select>
@@ -234,7 +271,11 @@ const BoardList = () => {
           />
         </div>
       </div>
-      <div className={`board-list ${filteredPosts?.length === 0 && !loading ? 'no-posts-container' : ''}`}>
+      <div
+        className={`board-list ${
+          filteredPosts?.length === 0 && !loading ? "no-posts-container" : ""
+        }`}
+      >
         {loading ? (
           <div className="loading">Loading...</div>
         ) : filteredPosts?.length > 0 ? (
@@ -274,6 +315,7 @@ const BoardList = () => {
           </button>
         </div>
       </div>
+      <div ref={ref}>여기에 스크롤을 내리면 "안녕"이라는 알림이 뜹니다.</div>
     </>
   );
 };
