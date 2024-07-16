7ff7f245139f8ba2e6fbf91d1dc2f60b3b2c81e0
Chaerin <chaexxrin@gmail.com>
2024-07-16 16:42:00 +0900

    css: toast 에러 css 수정

--- src/store/ProtectedChatRoute.jsx
+++ src/store/ProtectedChatRoute.jsx
@@ -15,12 +15,15 @@ const ProtectedChatRoute = () => {
       dispatch(setInitialState());
       dispatch(verifyToken()).unwrap()
       .catch(() => {
-        toast.error('채팅을 위해서는 로그인이 필요합니다.');
+        toast.error('채팅을 위해서는 로그인이 필요합니다.', {
+          autoClose: 3000 // 3초 동안 표시
+        });
         // 토큰 검증 실패 시 로그인 페이지로 리디렉션
         window.location.href = `/login?redirect=${encodeURIComponent(location.pathname)}`;
       });
     }
   }, [dispatch, isLoggedIn, user, location.pathname]);
+
 
   // 초기 상태에서는 콘텐츠를 렌더링
   return <Outlet />;
